1d36f5003f53a30d4cdc2512ab9851482c1c78ad
CenatoRxD <deychuk93@gmail.com>
2017-01-06 15:14:38 +0100

    #1 created main interface

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,16 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    public void addOnClick(MouseEvent mouseEvent) {
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("CrudFX");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setMinHeight(300);
+        primaryStage.setMinWidth(350);
         primaryStage.show();
     }
 
